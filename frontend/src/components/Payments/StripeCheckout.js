de5d9604ec90249bc87ed1e09a9d31ed60b9c0c2
DESKTOP-LA7NHF6\mauli <maulikkachchchi2000@gmail.com>
2022-09-13 09:21:04 +0530

    refactor: minor change

--- frontend/src/components/Payments/StripeCheckout.js
+++ frontend/src/components/Payments/StripeCheckout.js
@@ -5,8 +5,9 @@ export default function StripeCheckout({amount}) {
     return (
         <div>
             <StripeCheckoutButton amount={amount}>
-                <button className='btn btn-danger btn-sm ms-auto'>CHECK OUT</button>
-            </StripeCheckoutButton>
+             
+                <button className='btn btn-danger btn-sm ms-auto'>CHECK OUT</button> 
+            </StripeCheckoutButton> 
         </div>
     )
 }
